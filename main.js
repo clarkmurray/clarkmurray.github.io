f30f9a8ec5a2512a89f9ff1b40eaa9e2d1a7947c
Clark Murray <clarkamurray@gmail.com>
2017-12-06 15:41:33 -0500

    About Me section now more responsive

---
+++ main.js
@@ -0,0 +1,44 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() > window.innerHeight) {
+       $('nav').addClass('fixed');
+    }
+    else {
+       $('nav').removeClass('fixed');
+    }
+});
+
+
+$('body').scrollspy({ target: '#myNavbar' })
+
+// filter handling for a /dir/ OR /indexordefault.page
+function filterPath(string) {
+  return string
+    .replace(/^\//, '')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+    .replace(/\/$/, '');
+};
+
+$('a[href^="#"]').on('click',function (e) {
+     e.preventDefault();
+     var target = this.hash,
+     $target = $(target);
+     $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+     }, 500, 'swing', function () {
+          window.location.hash = target;
+     });
+});
+
+
+
+$('a[href^="#"]').click(function(){
+  var the_id = $(this).attr("href");
+
+      $('html, body').animate({
+          scrollTop:$(the_id).offset().top
+      }, 'slow');
+
+  return false;
+
+});
+
