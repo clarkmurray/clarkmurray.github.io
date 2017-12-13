8dcbb68af8cac67cde634fca5118fa5e46b78d4d
Clark Murray <clarkamurray@gmail.com>
2017-12-12 20:50:56 -0500

    More work on collapsible navbar

--- main.js
+++ main.js
@@ -42,3 +42,9 @@ $('a[href^="#"]').click(function(){
 
 });
 
+$(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
