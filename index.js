6f5ecb139dc86d2b3e3889811b33c3100bf083f1
Clark Murray <clarkamurray@gmail.com>
2018-02-15 23:56:16 -0500

    Collapsible navbar goes away on hash link

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var year = new Date();
 document.getElementById("year").innerHTML = year.getFullYear();
+
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
